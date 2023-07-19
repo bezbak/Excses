92a301a366e694af30ccb32048878185b4950102
itek <itybek@gmail.com>
2023-07-19 14:29:49 +0600

    test server

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from rest_framework import generics, viewsets
+from apps.users.models import User
+from apps.users.serializers import RegisterSerializer, UserSerializer
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    
