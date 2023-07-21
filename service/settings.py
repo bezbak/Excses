00137882a0c8a8ecc28db141ca8ba53d37d29a94
itek <itybek@gmail.com>
2023-07-22 05:28:17 +0600

    test static2

--- service/settings.py
+++ service/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 
 
 MEDIA_URL = 'media/'
