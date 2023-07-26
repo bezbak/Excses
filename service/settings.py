98b7f0b645c93e7ee9933495271c80e2a283de62
itek <itybek@gmail.com>
2023-07-26 15:59:23 +0600

    add swagger

--- service/settings.py
+++ service/settings.py
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #modules
+    'drf_yasg',
     "corsheaders",
     'rest_framework',
     'django_filters',
